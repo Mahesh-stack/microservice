3ac77ab43543129e469e03533cfab2b1dab7db76
MAHESH SUBHASH DEORAYE <maheshdevraye13@gmail.com>
2025-12-05 20:34:09 +0530

    Delete department-service directory

--- department-service/src/main/java/com/microservice/department/departmentservice/entity/Employee.java
+++
@@ -1,4 +0,0 @@
-package com.microservice.department.departmentservice.entity;
-
-public record Employee(Long id, Long departmentId, String name, int age, String position) {
-}
--- department-service/src/main/java/com/microservice/department/departmentservice/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.department.departmentservice.repository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository /*extends JpaRepository<Department, Integer>*/ {
-  /*  List<Department> department = new ArrayList<>();
-    Department findById(Long id);*/
-
-}
