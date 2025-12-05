cac0acbf0ade8d3570405724e0dcb627bf9f4269
MAHESH SUBHASH DEORAYE <maheshdevraye13@gmail.com>
2025-12-05 20:34:43 +0530

    Delete micro-service directory

--- micro-service/orderservice/src/main/java/com/micro/order/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.micro.order.repository;
-
-import com.micro.order.model.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Orders,Long> {
-}
